be0cc326bf797ce5147fc3e5f2c5f4e22b31f708
Gonzalo <gonzalodelpalacio@gmail.com>
2022-04-07 20:51:34 +0200

    Updates

--- static/js/4.26ad2ea4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoshtbattle-interface"]=this["webpackJsonpgoshtbattle-interface"]||[]).push([[4],{979:function(t,e){}}]);
-//# sourceMappingURL=4.26ad2ea4.chunk.js.map
---
+++ static/js/7.b3f31c7b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoshtbattle-interface"]=this["webpackJsonpgoshtbattle-interface"]||[]).push([[7],{460:function(t,e){}}]);
+//# sourceMappingURL=7.b3f31c7b.chunk.js.map
